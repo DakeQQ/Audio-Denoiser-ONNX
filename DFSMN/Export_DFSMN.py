09ee0368f46399d4b318a4eb2400dc7605fd0726
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-06-15 23:04:08 +0800

    Update Export_DFSMN.py

--- DFSMN/Export_DFSMN.py
+++ DFSMN/Export_DFSMN.py
@@ -55,7 +55,7 @@ class DFSMN(torch.nn.Module):
 
     def forward(self, audio):
         audio = audio.float()       # Don't divide by 32768.0
-        audio -= torch.mean(audio)  # Remove DC Offset
+        audio = audio - torch.mean(audio)  # Remove DC Offset
         audio = torch.cat((audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]), dim=-1)  # Pre Emphasize
         real_part, imag_part = self.stft_model(audio, 'constant')
         mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).transpose(1, 2).clamp(min=1e-5).log()

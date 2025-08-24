5190b46b093858a31c823846fd5f38a48b684536
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-08-24 11:33:08 +0800

    Update Optimize_ONNX.py

--- GTCRN/Optimize_ONNX.py
+++ GTCRN/Optimize_ONNX.py
@@ -39,7 +39,7 @@ slim(
 # transformers.optimizer
 model = optimize_model(optimized_model_path,
                        use_gpu=use_gpu_fp16,
-                       opt_level=99 if (target_platform == "amd64") and not use_gpu_fp16 else 2,
+                       opt_level=2,
                        num_heads=0,
                        hidden_size=0,
                        provider=provider,

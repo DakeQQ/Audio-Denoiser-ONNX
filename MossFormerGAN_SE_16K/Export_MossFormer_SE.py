7ee0f00ce85b20104051319ed63dbfa5eb8aea1a
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-08-24 11:01:45 +0800

    Update Export_MossFormer_SE.py

--- MossFormerGAN_SE_16K/Export_MossFormer_SE.py
+++ MossFormerGAN_SE_16K/Export_MossFormer_SE.py
@@ -12,22 +12,26 @@ from pydub import AudioSegment
 from STFT_Process import STFT_Process  # The custom STFT/ISTFT can be exported in ONNX format.
 
 
-model_path = "/home/DakeQQ/Downloads/MossFormerGAN_SE_16K"
+model_path = "/home/DakeQQ/Downloads/MossFormerGAN_SE_16K"                           # The MossFormerGAN_SE_16K download folder
 onnx_model_A = "/home/DakeQQ/Downloads/MossFormer_ONNX/MossFormerGAN_SE_16K.onnx"    # The exported onnx model path.
 test_noisy_audio = "./examples/speech_with_noise1.wav"                               # The noisy audio path.
 save_denoised_audio = "./examples/speech_with_noise1_denoised.wav"                   # The output denoised audio path.
 
 
 DYNAMIC_AXES = False                    # The default dynamic_axes is the input audio length. Note that some providers only support static axes.
+KEEP_ORIGINAL_SAMPLE_RATE = False       # If False, the model outputs audio at 16kHz; otherwise, it uses the original sample rate.
+SAMPLE_RATE = 16000                     # [8000, 16000, 22500, 24000, 44000, 48000]; It accepts various sample rates as input.
 INPUT_AUDIO_LENGTH = 16000              # Maximum input audio length: the length of the audio input signal (in samples) is recommended to be greater than 8000 and less than 96000. Higher values yield better quality but time consume. It is better to set an integer multiple of the NFFT value.
-MAX_SIGNAL_LENGTH = 1024 if DYNAMIC_AXES else (INPUT_AUDIO_LENGTH // 100 + 1)  # Max frames for audio length after STFT processed. Set a appropriate larger value for long audio input, such as 4096.
+MAX_SIGNAL_LENGTH = 2048 if DYNAMIC_AXES else (INPUT_AUDIO_LENGTH // 50 + 1)  # Max frames for audio length after STFT processed. Set a appropriate larger value for long audio input, such as 4096.
 WINDOW_TYPE = 'hamming'                 # Type of window function used in the STFT
 N_MELS = 60                             # Number of Mel bands to generate in the Mel-spectrogram
 NFFT = 400                              # Number of FFT components for the STFT process
 WINDOW_LENGTH = 400                     # Length of windowing, edit it carefully.
 HOP_LENGTH = 100                        # Number of samples between successive frames in the STFT
-SAMPLE_RATE = 16000                     # The MossFormer_SE parameter, do not edit the value.
 MAX_THREADS = 4                         # Number of parallel threads for test audio denoising.
+
+
+SAMPLE_RATE_SCALE = float(16000.0 / SAMPLE_RATE)
 
 
 site_package_path = site.getsitepackages()[-1]
@@ -47,7 +51,7 @@ def normalize_to_int16(audio):
 
 
 class MOSSFORMER_SE(torch.nn.Module):
-    def __init__(self, mossformer_se, stft_model, istft_model):
+    def __init__(self, mossformer_se, stft_model, istft_model, sample_rate):
         super(MOSSFORMER_SE, self).__init__()
         self.mossformer_se = mossformer_se
         self.stft_model = stft_model
@@ -55,45 +59,66 @@ class MOSSFORMER_SE(torch.nn.Module):
         self.compress_factor = float(0.3)
         self.compress_factor_inv = float(0.5 / self.compress_factor) - 0.5
         self.compress_factor_sqrt = float(self.compress_factor * 0.5)
+        self.sample_rate = sample_rate
 
     def forward(self, audio):
-        # Initial preprocessing
         audio = audio.float()
-        norm_factor = torch.sqrt(torch.mean(audio * audio, dim=-1, keepdim=True) + 1e-6)
-
-        # STFT and magnitude/phase extraction
-        real_part, imag_part = self.stft_model(audio / norm_factor, 'constant')
+        if SAMPLE_RATE_SCALE < 1.0:
+            norm_factor = torch.sqrt(torch.mean(audio * audio, dim=-1, keepdim=True) + 1e-6)
+            audio /= norm_factor
+            if self.sample_rate != 16000:
+                audio = torch.nn.functional.interpolate(
+                    audio,
+                    scale_factor=SAMPLE_RATE_SCALE,
+                    mode='linear',
+                    align_corners=True
+                )
+        else:
+            if self.sample_rate != 16000:
+                audio = torch.nn.functional.interpolate(
+                    audio,
+                    scale_factor=SAMPLE_RATE_SCALE,
+                    mode='linear',
+                    align_corners=True
+                )
+            norm_factor = torch.sqrt(torch.mean(audio * audio, dim=-1, keepdim=True) + 1e-6)
+            audio /= norm_factor
+        real_part, imag_part = self.stft_model(audio, 'constant')
         power = real_part * real_part + imag_part * imag_part
         magnitude_compress = torch.pow(power, self.compress_factor_sqrt)
         inv_magnitude = torch.pow(power, -0.5) * magnitude_compress
         cos_phase = real_part * inv_magnitude
         sin_phase = imag_part * inv_magnitude
-
-        # Prepare input for model
         x_in = torch.cat([magnitude_compress, cos_phase, sin_phase], dim=0).unsqueeze(0)
-
-        # MossFormer processing
         x = self.mossformer_se.dense_encoder(x_in.transpose(-1, -2))
         for ii in range(self.mossformer_se.n_layers):
             x = self.mossformer_se.blocks[ii](x)
-
-        # Generate outputs
         mask = self.mossformer_se.mask_decoder(x).transpose(1, 2)
         complex_out = self.mossformer_se.complex_decoder(x).transpose(-1, -2)
-
-        # Apply mask and combine with complex output
         mag_real = mask * cos_phase
         mag_imag = mask * sin_phase
-
         final_real = mag_real + complex_out[:, 0]
         final_imag = mag_imag + complex_out[:, 1]
-
-        # Decompress and reconstruct
         factor = torch.pow(final_real * final_real + final_imag * final_imag, self.compress_factor_inv)
-
-        # ISTFT and final processing
-        audio = self.istft_model(final_real * factor, final_imag * factor) * norm_factor
-
+        audio = self.istft_model(final_real * factor, final_imag * factor)
+        if SAMPLE_RATE_SCALE < 1.0:
+            audio *= norm_factor
+            if KEEP_ORIGINAL_SAMPLE_RATE and self.sample_rate != 16000:
+                audio = torch.nn.functional.interpolate(
+                    audio,
+                    scale_factor=1.0 / SAMPLE_RATE_SCALE,
+                    mode='linear',
+                    align_corners=True
+                )
+        else:
+            if KEEP_ORIGINAL_SAMPLE_RATE and self.sample_rate != 16000:
+                audio = torch.nn.functional.interpolate(
+                    audio,
+                    scale_factor=1.0 / SAMPLE_RATE_SCALE,
+                    mode='linear',
+                    align_corners=True
+                )
+            audio *= norm_factor
         return (audio.clamp(min=-32768.0, max=32767.0)).to(torch.int16)
 
 
@@ -104,8 +129,9 @@ with torch.inference_mode():
 
     myClearVoice = ClearVoice(task='speech_enhancement', model_names=['MossFormerGAN_SE_16K'], model_path=model_path)
     mossformer = myClearVoice.models[0].model.eval().float().to("cpu")
-    mossformer = MOSSFORMER_SE(mossformer, custom_stft, custom_istft)
+    mossformer = MOSSFORMER_SE(mossformer, custom_stft, custom_istft, SAMPLE_RATE)
     audio = torch.ones((1, 1, INPUT_AUDIO_LENGTH), dtype=torch.int16)
+
     torch.onnx.export(
         mossformer,
         (audio,),
@@ -153,18 +179,14 @@ print(f"\nTest Input Audio: {test_noisy_audio}")
 audio = np.array(AudioSegment.from_file(test_noisy_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
 audio = normalize_to_int16(audio)
 audio_len = len(audio)
-inv_audio_len = float(100.0 / audio_len)
 audio = audio.reshape(1, 1, -1)
 shape_value_in = ort_session_A._inputs_meta[0].shape[-1]
-shape_value_out = ort_session_A._outputs_meta[0].shape[-1]
 if isinstance(shape_value_in, str):
-    INPUT_AUDIO_LENGTH = min(32000, audio_len)
+    INPUT_AUDIO_LENGTH = max(96000, audio_len)
 else:
     INPUT_AUDIO_LENGTH = shape_value_in
 stride_step = INPUT_AUDIO_LENGTH
 if audio_len > INPUT_AUDIO_LENGTH:
-    if (shape_value_in != shape_value_out) & isinstance(shape_value_in, int) & isinstance(shape_value_out, int):
-        stride_step = shape_value_out
     num_windows = int(np.ceil((audio_len - INPUT_AUDIO_LENGTH) / stride_step)) + 1
     total_length_needed = (num_windows - 1) * stride_step + INPUT_AUDIO_LENGTH
     pad_amount = total_length_needed - audio_len
@@ -176,6 +198,12 @@ elif audio_len < INPUT_AUDIO_LENGTH:
     white_noise = (np.sqrt(np.mean(audio_float * audio_float, dtype=np.float32), dtype=np.float32) * np.random.normal(loc=0.0, scale=1.0, size=(1, 1, INPUT_AUDIO_LENGTH - audio_len))).astype(audio.dtype)
     audio = np.concatenate((audio, white_noise), axis=-1)
 aligned_len = audio.shape[-1]
+inv_audio_len = float(100.0 / audio_len)
+
+
+if SAMPLE_RATE != 16000 and not KEEP_ORIGINAL_SAMPLE_RATE:
+    SAMPLE_RATE = 16000
+    audio_len = int(audio_len * SAMPLE_RATE_SCALE)
 
 
 def process_segment(_inv_audio_len, _slice_start, _slice_end, _audio):
@@ -199,7 +227,7 @@ with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:  # Parallel denois
         print(f"Complete: {results[-1][0]:.3f}%")
 results.sort(key=lambda x: x[0])
 saved = [result[1] for result in results]
-denoised_wav = np.concatenate(saved, axis=-1)[0, 0, :audio_len]
+denoised_wav = np.concatenate(saved, axis=-1).reshape(-1)[:audio_len]
 end_time = time.time()
 print(f"Complete: 100.00%")
 

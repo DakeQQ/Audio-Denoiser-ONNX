417858516fd5cbd80094fd69f376ba973d37a3f8
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-08-24 10:10:51 +0800

    Update Export_MossFormer_SE.py

--- MossFormer2_SE_48K/Export_MossFormer_SE.py
+++ MossFormer2_SE_48K/Export_MossFormer_SE.py
@@ -19,8 +19,8 @@ save_denoised_audio = "./examples/speech_with_noise1_denoised.wav"              
 
 
 DYNAMIC_AXES = False                    # The default dynamic_axes is the input audio length. Note that some providers only support static axes.
-KEEP_ORIGINAL_SAMPLE_RATE = False       # If `False`, the model will output the 48K audio else original sample rate.
-SAMPLE_RATE = 48000                     # [16000, 22500, 24000, 48000]; The MossFormer_SE parameter. It accepts various sample rate as inputs.
+KEEP_ORIGINAL_SAMPLE_RATE = False       # If False, the model outputs audio at 16kHz; otherwise, it uses the original sample rate.
+SAMPLE_RATE = 48000                     # [8000, 16000, 22500, 24000, 44000, 48000]; It accepts various sample rates as input.
 INPUT_AUDIO_LENGTH = 48000              # Maximum input audio length: the length of the audio input signal (in samples) is recommended to be greater than 8000 and less than 96000. Higher values yield better quality but time consume. It is better to set an integer multiple of the NFFT value.
 MAX_SIGNAL_LENGTH = 4096 if DYNAMIC_AXES else (INPUT_AUDIO_LENGTH // 100 + 1)  # Max frames for audio length after STFT processed. Set a appropriate larger value for long audio input, such as 4096.
 WINDOW_TYPE = 'hamming'                 # Type of window function used in the STFT

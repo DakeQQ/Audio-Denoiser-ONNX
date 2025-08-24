edea3977ad54bc72339b792e4f6415c972b594e9
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-08-24 12:49:48 +0800

    Update Inference_MossFormer_SE_ONNX.py

--- MossFormerGAN_SE_16K/Inference_MossFormer_SE_ONNX.py
+++ MossFormerGAN_SE_16K/Inference_MossFormer_SE_ONNX.py
@@ -113,7 +113,7 @@ audio = audio.reshape(1, 1, -1)
 shape_value_in = ort_session_A._inputs_meta[0].shape[-1]
 shape_value_out = ort_session_A._outputs_meta[0].shape[-1]
 if isinstance(shape_value_in, str):
-    INPUT_AUDIO_LENGTH = min(24000, audio_len)
+    INPUT_AUDIO_LENGTH = min(6 * SAMPLE_RATE, audio_len)
 else:
     INPUT_AUDIO_LENGTH = shape_value_in
 stride_step = INPUT_AUDIO_LENGTH
